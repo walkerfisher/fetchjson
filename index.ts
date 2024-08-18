0fe4182a9baf93798bc5cc7b88f21286257f25fb
Walker Fisher <walkerfisher@walkers-air.mynetworksettings.com>
2024-08-17 21:24:54 -0700

    fetch json demo

---
+++ index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(response => {
+  const todo: Todo = response.data;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+function logTodo(id: number, title: string, completed: boolean){
+  console.log(`
+  The todo id is: ${id}
+  with a title: ${title}
+  and is it completed?: ${completed}
+  `);
+}
